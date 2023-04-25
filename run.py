02e2cd7ddcd7822f989180f1dc78dccfc4ad0e85
Herald Yu <yuhr123@gmail.com>
2023-04-25 10:54:34 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import subprocess
 
 # Prompt user for input
 email = input("请输入您的电子邮件地址：")
@@ -53,16 +52,49 @@ COPY --from=builder /usr/bin/caddy /usr/bin/caddy
 # Write Dockerfile
 with open("Dockerfile", "w") as f:
     f.write(dockerfile)
+    
+# Generate systemd service file
+service_file = f"""
+Documentation=man:podman-generate-systemd(1)
+Wants=network-online.target
+After=network-online.target
+RequiresMountsFor=%t/containers
 
-# Prompt user to build Caddy image
-if input("是否编译 Caddy 镜像？(y/n)").lower() == "y":
+[Service]
+Environment=PODMAN_SYSTEMD_UNIT=%n
+Restart=on-failure
+TimeoutStopSec=70
+ExecStartPre=/bin/rm -f %t/%n.ctr-id
+ExecStart=/usr/bin/podman run \\
+       	--cidfile=%t/%n.ctr-id \\
+       	--cgroups=no-conmon \\
+       	--rm \\
+       	--sdnotify=conmon \\
+       	--replace \\
+       	-d \\
+       	--name caddy \\
+       	-v {os.getcwd()}/Caddyfile:/etc/caddy/Caddyfile:Z \\
+       	-v caddy_data:/data \\
+       	-v caddy_conf:/config \\
+       	-p 80:80 \\
+       	-p 443:443 naive/caddy
+ExecStop=/usr/bin/podman stop --ignore --cidfile=%t/%n.ctr-id
+ExecStopPost=/usr/bin/podman rm -f --ignore --cidfile=%t/%n.ctr-id
+Type=notify
+NotifyAccess=all
+
+[Install]
+WantedBy=default.target
+"""
+
+with open("/lib/systemd/system/container-caddy.service", "w") as f:
+    f.write(service_file)
+
+# Build image and start caddy service
+confirm_build = input("是否编译镜像？(y/n)")
+if confirm_build.lower() == "y":
     os.system("sudo podman pull caddy")
     os.system("sudo podman build -t localhost/naive/caddy .")
-
-# Prompt user to run Caddy container
-if input("是否运行 Caddy 容器？(y/n) ").lower() == "y":
-    os.system("sudo podman rm -f caddy")
-    subprocess.run(["sudo", "podman", "run", "-d", "--name", "caddy", 
-        "-v", f"{os.getcwd()}/Caddyfile:/etc/caddy/Caddyfile:Z", "-v", 
-        "caddy_data:/data", "-v", "caddy_conf:/config", "-p", "80:80", 
-        "-p", "443:443", "naive/caddy"])
+    confirm_start = input("是否启动 caddy 并设置为开机自动启动？(y/n)")
+    if confirm_start.lower() == "y":
+        os.system("sudo systemctl enable --now container-caddy.service")
